73c744958b03d29f1a747253832f66d07c0af5d3
oriver <fmzk_kzm@yahoo.co.jp>
2018-08-26 15:12:21 +0900

    user

--- 00_Python/autologin/cybouze_login.py
+++ 00_Python/autologin/cybouze_login.py
@@ -7,24 +7,24 @@ from time import sleep
 
 
 # Variable
-_url = "https://cybozulive.com/"
-_id = ["mail-address]
-_pass = ["xxxxxxxxx"]
+URL = "https://cybozulive.com/"
+ID = ["mail-address"]
+PASS = ["xxxxxxxxx"]
 
 
 # Login
 def login_():
 	
     # ChromeStarting
-    _driver = webdriver.Chrome("/Users/kozakai/bin/python/chromedriver")
+    _driver = webdriver.Chrome("./chromedriver")
     _driver.set_window_size(1280, 720)
-    _driver.get(_url)
+    _driver.get(URL)
 
     # Login
     _login_id = _driver.find_element_by_name("loginMailAddress")
     _password = _driver.find_element_by_name("password")
-    _login_id.send_keys(_id[_num])
-    _password.send_keys(_pass[_num])
+    _login_id.send_keys(ID[_num])
+    _password.send_keys(PASS[_num])
     _driver.find_element_by_name(
                                 "czJzdHJ1dHMuQkFTRTY0X0ZPUk1BVDpzMnN0cnV0cy5BQ1RJT05fRVhQUkVTU0lPTj0lNDAlN0Jsb2dpbl9sb2dpbkFjdGlvbi5kb0xvZ2luJTdE").click()
 
@@ -40,7 +40,7 @@ def login_():
 # Main #####################
 if __name__ == '__main__':
     _num = 0
-    for _loop in _id:
+    for _loop in ID:
         login_()
         sleep(3)
         _num += 1
